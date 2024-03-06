bf38766ecced3df1cb3d3ce47e3b3e122d185eb7
Luan Lima <lubasantonio23@gmail.com>
2024-03-05 23:35:29 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/lubas/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.lubas.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lubas.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	
+	
+}
