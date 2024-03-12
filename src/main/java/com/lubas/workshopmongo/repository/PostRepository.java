d4aea6199a429084eb7f7b0961aa21309679bf9c
Luan Lima <lubasantonio23@gmail.com>
2024-03-11 22:09:58 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/lubas/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.lubas.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lubas.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+	
+}
