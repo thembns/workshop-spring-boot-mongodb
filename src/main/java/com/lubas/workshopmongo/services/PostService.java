902b7b823a3528ac94cba3a3fae7ac5fabf8616c
Luan Lima <lubasantonio23@gmail.com>
2024-03-12 22:50:14 -0300

    Consulta simples com query methods

--- src/main/java/com/lubas/workshopmongo/services/PostService.java
+++ src/main/java/com/lubas/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.lubas.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
